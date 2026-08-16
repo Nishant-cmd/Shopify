ace89ec84a93e7d1aec7af74c53589062ab87dd7
Nishant-cmd <140052206+Nishant-cmd@users.noreply.github.com>
2026-08-16 12:22:04 +0545

    feat: Shop page initial implementation

--- src/components/pages/Shop.jsx
+++ src/components/pages/Shop.jsx
@@ -1,3 +1,96 @@
+import { NavLink } from 'react-router';
+import styles from '../../styles/shop.module.css';
+
 export default function Shop() {
-  return <div>This will render Shop data.</div>;
+  return (
+    <main className={styles.main}>
+      <Categories />
+      <section className={styles.section}>
+        <CategoryTitle />
+        <ShowProducts />
+      </section>
+    </main>
+  );
+}
+
+function Categories() {
+  return (
+    <aside className={styles.aside}>
+      <h4>Categories</h4>
+      <nav className={styles.nav}>
+        <NavLink
+          to="/shop/all"
+          className={styles.link}
+          style={({ isActive }) => {
+            return {
+              backgroundColor: isActive ? 'var(--accent)' : '',
+              color: isActive ? 'var(--bg-main)' : '',
+            };
+          }}
+        >
+          All Products
+        </NavLink>
+        <NavLink
+          to="/shop/electronics"
+          className={styles.link}
+          style={({ isActive }) => {
+            return {
+              backgroundColor: isActive ? 'var(--accent)' : '',
+              color: isActive ? 'var(--bg-main)' : '',
+            };
+          }}
+        >
+          Electronics
+        </NavLink>
+        <NavLink
+          to="/shop/mens"
+          className={styles.link}
+          style={({ isActive }) => {
+            return {
+              backgroundColor: isActive ? 'var(--accent)' : '',
+              color: isActive ? 'var(--bg-main)' : '',
+            };
+          }}
+        >
+          Men's Clothing
+        </NavLink>
+        <NavLink
+          to="/shop/women"
+          className={styles.link}
+          style={({ isActive }) => {
+            return {
+              backgroundColor: isActive ? 'var(--accent)' : '',
+              color: isActive ? 'var(--bg-main)' : '',
+            };
+          }}
+        >
+          Women's Clothing
+        </NavLink>
+        <NavLink
+          to="/shop/jewelery"
+          className={styles.link}
+          style={({ isActive }) => {
+            return {
+              backgroundColor: isActive ? 'var(--accent)' : '',
+              color: isActive ? 'var(--bg-main)' : '',
+            };
+          }}
+        >
+          Jewelery
+        </NavLink>
+      </nav>
+    </aside>
+  );
+}
+function CategoryTitle() {
+  return (
+    <header className={styles.header}>
+      <h3>Men's Clothing</h3>
+      <p>4 Products</p>
+    </header>
+  );
+}
+
+function ShowProducts() {
+  return <div>This will show product details</div>;
 }
